3f57f0489950e382b13832560eeddbe4eea6bcb7
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-05-20 14:55:27 +0700

    bug

---
+++ modul1/modul1_7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int a = 4, b = 3, c = 2, d = 1; 
+int aa = 5, bb = 4, cc = 3, dd = 7, y;
+float hasil;
+int main(){
+    cout << "Y = 5x^4 + 3x^3 - x^2 + 7" << endl;
+    y = (aa*a)*pow(x,(a-1)) + (bb*b)*pow(x,(b-1)) - (cc*c)*pow(x,(c-1)) + (dd*d)*pow(x,(d-1));
+    cout << y;
+}
