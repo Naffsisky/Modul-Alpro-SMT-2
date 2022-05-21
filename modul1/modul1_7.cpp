4b553927c96cc5883dd6cc68cca64215eaa08d31
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-05-21 23:09:15 +0700

    Tugas Modul 1

--- modul1/modul1_7.cpp
+++ modul1/modul1_7.cpp
@@ -8,9 +8,9 @@ int main(){
     n[1] =5; n[2] =3; n[3] =-1; 
     pangkat[1] = 4; pangkat[2] = 3; pangkat[3] = 2;
     
-for (int i = 1; i <= 3; i++){
-    cout << n[i]*pangkat[i] << ".X^" << pangkat[i] - 1 ;
-    if (i < 3) cout << " + ";
-    else cout << endl;
+    for (int i = 1; i <= 3; i++){
+        cout << n[i]*pangkat[i] << ".X^" << pangkat[i] - 1 ;
+        if (i < 3) cout << " + ";
+        else cout << endl;
     }
 }
