4f750e28ec3a2cbc5e4d67187932c49e047c3ca1
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-06-26 22:05:38 +0700

    Tugas modul 6

--- modul6/modul6_5.cpp
+++ modul6/modul6_5.cpp
@@ -1,22 +1,61 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    string namadepan[100], namabelakang[100], namalengkap[100];
-    int i, n;
+int pilihan, jml_nama, n_nama;
+string kembali;
 
-    cout << "Masukan banyak data : ";
-    cin >> n;
-    for (i = 1; i <= n; i++){
-        cout << "Masukan nama depan ke " << i << " : ";
-        cin >> namadepan[i];
-        cout << "Masukan nama belakang ke " << i << " : ";
-        cin >> namabelakang[i];
+struct Nama {
+    int id;
+    char nama_depan[50];
+    char nama_belakang[50];
+} s_nama[100];
+
+int main(){
+    menu:
+    cout << "\tMenu\n";
+    cout << "1. Tambah nama\n";
+    cout << "2. Lihat nama\n";
+    cout << "Masukan pilihan : ";
+    cin >> pilihan;
+    switch(pilihan){
+        case 1:
+        tambah_anggota:
+            jml_nama += 0;
+            s_nama[jml_nama].id = jml_nama;
+            cout << "\tTambah Nama\n";
+            cout << "Nama depan\t: ";
+            cin >> s_nama[jml_nama].nama_depan;
+            cout << "Nama belakang\t: ";
+            cin >> s_nama[jml_nama].nama_belakang;
+
+            jml_nama++;
+            cout <<  "Data Anda Sukses Ditambahkan\n\n";
+            cout << "Kembali ke manu? y/n\n";
+            cout << "Masukan pilihan : ";
+            cin >> kembali;  
+            if(kembali == "Y" || kembali == "y"){
+                goto menu;
+            }
+            else{
+                exit(0);
+            }  
+            break;
+        case 2:
+        cout << "\nMengecek Data Anggota\n";
+            log_nama:
+            for(n_nama = 0; n_nama < jml_nama; n_nama++){
+                cout << "Nama anggota\t: " << s_nama[n_nama].nama_depan << endl;
+                cout << "Prodi anggota\t: " << s_nama[n_nama].nama_belakang << endl << endl;
+            }
+            cout << "Kembali ke menu? y/n\n";
+            cout << "Masukan pilihan : ";
+            cin >> kembali;    
+            if(kembali == "Y" || kembali == "y"){
+                goto menu;
+            }
+            else{ 
+                exit(0);
+            }
+            break;
     }
-    cout << "\n\nData : \n";
-    for (i = 1; i <= n; i++){
-        cout << i << ". " << namadepan[i] << " "; 
-        cout << namabelakang[i] << "\n";
-    }
-    return 0;
 } 
